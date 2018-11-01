47bf5e18325766a1703b9079632000f3b2067817
Anderson <Marcos@MARCOS-PC>
2018-11-01 07:38:48 -0200

    só coloquei a classe program pra abrir o splash

--- RestauranteADM/RestauranteADM/Program.cs
+++ RestauranteADM/RestauranteADM/Program.cs
@@ -25,7 +25,7 @@ namespace RestauranteADM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new Cadastrar_cliente());
+            Application.Run(new splash());
         }
     }
 }
